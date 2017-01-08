e80f5284b1c1085a820fae75aeaa54a518b7aa6f
mryamz <mryamz@users.noreply.github.com>
2017-01-08 08:35:35 -0800

    Ugly AF

--- Lumber Jack/js/movement.js
+++
@@ -1,9 +0,0 @@
-var Movement = {
-    update: function (data) {
-        Movement.jack(data);
-    },
-
-    jack: function (data) {
-        data.entities.jack.currentState.movement(data);
-    }
-};
