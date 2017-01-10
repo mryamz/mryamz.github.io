063a8cba24f506b5a1c1098ea9b47c999cea6150
mryamz <mryamz@users.noreply.github.com>
2017-01-10 04:37:42 -0800

    Pulse
    mryamz lighter bg

--- js/banner/banner.js
+++ js/banner/banner.js
@@ -2,10 +2,16 @@ var Banner = {
 	
 	canvas: null,
 	context: null,
+	init: null,
+	period: null,
+
 		
 	init: function(){ 
 		this.canvas = document.getElementById('canvas_banner');
 		this.context = canvas.getContext('2d');		
+		this.init = new Date().getTime();
+		this.period = 6000;
+		
 		setInterval(Banner.loop, 1000/30);		
 		particleManager.init(500, canvas.height);
 	},
@@ -19,14 +25,18 @@ var Banner = {
 		context.fillStyle = "#F2F2F2";
 		context.fillRect(0, 0, canvas.width, canvas.height);
 		
-		// draw particles
-		particleManager.draw(context);
+		var elapsed = (new Date().getTime() - init);
+		var a = Math.abs(Math.sin(Math.PI * elapsed / period)) / 3;
 		
-		context.fillStyle = "#000000"
-		context.font = "75px Arial"
-		var string = "mryamz"
+		context.fillStyle = "rgba(0, 0, 0, " + a +")";
+		context.font = "75px Arial";
+		var string = "mryamz ";
 		var length = context.measureText(string).width;
 		context.fillText(string, w / 2 - length / 2, 100);
+		
+				
+		// draw particles
+		particleManager.draw(context);
 
 		context.closePath();		
 	}
